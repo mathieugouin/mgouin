b8e663a5cc16ec71e1e7064eee6fb1b3a3689b73
mgouin@gmail.com <mgouin@gmail.com@1737b206-0a78-cc10-30ec-52fab7fe60c2>
2013-07-15 21:56:14 +0000

    metar

---
+++ mgouin.py
@@ -0,0 +1,61 @@
+import webapp2
+import urllib
+import re
+
+import mgouinlib as MGL
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+        # parameter:
+        #  txtweb-verifyid
+        #   The txtweb-verifyid string that was sent to your app
+        #
+        #  txtweb-message
+        #   The txtweb-message string that was sent to your app
+        #
+        #  txtweb-mobile
+        #   The txtweb-mobile string that was sent to your app
+        #
+        #  txtweb-protocol
+        #   The txtweb-protocol string that was sent to your app
+        #
+        #
+        #   http://weather.noaa.gov/pub/data/observations/metar/stations/CYHU.TXT
+        #   http://weather.rap.ucar.edu/surface/stations.txt
+        #   http://localhost:8080/?txtweb-message=CYHU
+        #   http://mgouin.appspot.com/?txtweb-message=CYHU
+
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write('<html><head><title>METAR</title><meta name="txtweb-appkey" content="9dd9146a-ad9f-4dea-b38f-065b4165b357" /></head>\n<body>\n')
+        #self.response.write("<div>MGouin App</div>")
+
+        #for a in self.request.arguments():
+        #    self.response.write("<div>Argument [" + a + "] = [" + self.request.get(a) + "]</div>")
+
+        station = self.request.get("txtweb-message").upper()
+        #self.response.write("<div>Asked for station: [" + station + "]</div>")
+
+        lines = MGL.metarHandler(station)
+
+        # Write response
+        for l in lines:
+            self.response.write(MGL.processLine(l))
+
+        self.response.write(r"""<script>""")
+        self.response.write(r"""  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){""" + "\n")
+        self.response.write(r"""  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),""" + "\n")
+        self.response.write(r"""  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)""" + "\n")
+        self.response.write(r"""  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');""" + "\n")
+        self.response.write(r"""  ga('create', 'UA-1787000-3', 'mgouin.appspot.com');""" + "\n")
+        self.response.write(r"""  ga('send', 'pageview');""" + "\n")
+        self.response.write(r"""</script>""" + "\n")
+
+        self.response.write("\n</body>\n</html>\n")
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+
