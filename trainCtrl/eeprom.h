88776c364aae08980522cf2adc7cd25b92e919c9
mgouin@gmail.com <mgouin@gmail.com@1737b206-0a78-cc10-30ec-52fab7fe60c2>
2015-03-04 21:01:19 +0000

    c

--- trainCtrl/eeprom.h
+++ trainCtrl/eeprom.h
@@ -1,9 +1,12 @@
+/**
+ * I2C serial EEPROM
+ */
+
 #ifndef I2C_EEPROM_H
 #define I2C_EEPROM_H
 
 #include "common.h"
 
-// serial i2c eeprom functions:
 void i2c_write_eeprom(uint16_t address, uint8_t data);
 uint8_t i2c_read_eeprom(uint16_t address);
 
