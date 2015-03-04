e61c3135c8dab5f6857a3f905abfaf972a04df8a
mgouin@gmail.com <mgouin@gmail.com@1737b206-0a78-cc10-30ec-52fab7fe60c2>
2015-03-04 20:57:35 +0000

    c

--- trainCtrl/buffer.h
+++ trainCtrl/buffer.h
@@ -21,10 +21,10 @@ typedef enum
 
 typedef struct
 {
-  bufferData_t *data;
-  bufferIndex_t size;
-  bufferIndex_t putIdx;
-  bufferIndex_t getIdx;
+  bufferData_t *data;   // ptr to data storage
+  bufferIndex_t size;   // buffer size (related to *data)
+  bufferIndex_t putIdx; // put => write index
+  bufferIndex_t getIdx; // get => read index
 } bufferStruct_t;
 
 bufferStatus_t bufferInit(bufferStruct_t* iopBuf, bufferData_t *bufferStorage, bufferIndex_t size);
